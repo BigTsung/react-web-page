1c9b3ccde1eb8853ce8923ae06032fb7e84f2995
BigTsung <yutsungchiu@gmail.com>
2020-04-13 10:25:02 +0800

    Updates

---
+++ precache-manifest.365ef8002802ad04cd7837d258d37f7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a1c124631a91262efe57c427f10b10",
+    "url": "/react-web-page/index.html"
+  },
+  {
+    "revision": "3dd41769a2fe5adbe124",
+    "url": "/react-web-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c1946b7abb2b4a4a92c4",
+    "url": "/react-web-page/static/js/2.23dddbaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-web-page/static/js/2.23dddbaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd41769a2fe5adbe124",
+    "url": "/react-web-page/static/js/main.808b1b49.chunk.js"
+  },
+  {
+    "revision": "de52c027d1315b25aaea",
+    "url": "/react-web-page/static/js/runtime-main.64bf27b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-web-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
